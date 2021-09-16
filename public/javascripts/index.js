762f4583dc309ad5242e9dc1d77f20779b684ef2
Yining Wang <ywang625@sheffield.ac.uk>
2021-09-16 22:54:34 +0800

    first commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,79 @@
+let name = null;
+let roomNo = null;
+let socket=null;
+
+
+/**
+ * called by <body onload>
+ * it initialises the interface and the expected socket messages
+ * plus the associated actions
+ */
+function init() {
+    // it sets up the interface so that userId and room are selected
+    document.getElementById('initial_form').style.display = 'block';
+    document.getElementById('chat_interface').style.display = 'none';
+
+    //@todo here is where you should initialise the socket operations as described in teh lectures (room joining, chat message receipt etc.)
+}
+
+/**
+ * called to generate a random room number
+ * This is a simplification. A real world implementation would ask the server to generate a unique room number
+ * so to make sure that the room number is not accidentally repeated across uses
+ */
+function generateRoom() {
+    roomNo = Math.round(Math.random() * 10000);
+    document.getElementById('roomNo').value = 'R' + roomNo;
+}
+
+/**
+ * called when the Send button is pressed. It gets the text to send from the interface
+ * and sends the message via  socket
+ */
+function sendChatText() {
+    let chatText = document.getElementById('chat_input').value;
+    // @todo send the chat message
+}
+
+/**
+ * used to connect to a room. It gets the user name and room number from the
+ * interface
+ */
+function connectToRoom() {
+    roomNo = document.getElementById('roomNo').value;
+    name = document.getElementById('name').value;
+    let imageUrl= document.getElementById('image_url').value;
+    if (!name) name = 'Unknown-' + Math.random();
+    //@todo join the room
+    initCanvas(socket, imageUrl);
+    hideLoginInterface(roomNo, name);
+}
+
+/**
+ * it appends the given html text to the history div
+ * this is to be called when the socket receives the chat message (socket.on ('message'...)
+ * @param text: the text to append
+ */
+function writeOnHistory(text) {
+    if (text==='') return;
+    let history = document.getElementById('history');
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = text;
+    history.appendChild(paragraph);
+    // scroll to the last element
+    history.scrollTop = history.scrollHeight;
+    document.getElementById('chat_input').value = '';
+}
+
+/**
+ * it hides the initial form and shows the chat
+ * @param room the selected room
+ * @param userId the user name
+ */
+function hideLoginInterface(room, userId) {
+    document.getElementById('initial_form').style.display = 'none';
+    document.getElementById('chat_interface').style.display = 'block';
+    document.getElementById('who_you_are').innerHTML= userId;
+    document.getElementById('in_room').innerHTML= ' '+room;
+}
+
---
+++ socket.io/socket-io.js
@@ -0,0 +1,9 @@
+
+exports.init = function(io) {
+  io.sockets.on('connection', function (socket) {
+    try {
+     // insert here your event
+    } catch (e) {
+    }
+  });
+}
