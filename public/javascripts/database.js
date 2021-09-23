c756eb821afda67d20b291dbcba9d9007164331a
Yining Wang <ywang625@sheffield.ac.uk>
2021-09-23 20:16:28 +0800

    add the function init the database

--- public/javascripts/database.js
+++ public/javascripts/database.js
@@ -2,38 +2,28 @@ import * as idb from './idb/index.js';
 
 let db;
 const DB_NAME = 'db_data';
-const STORE_NAME_1 = 'data_userChats';
-const STORE_NAME_2 = 'data_annotations';
+const STORE_NAME = 'data';
 
 /**
- * init 2 databases
- * the first one stores the chat records
- * the second one stores the annotations (including the image)
+ * init the databases
+ * stores the chat records and annotations
  */
 
 async function initDatabase() {
-    if(db == null){
-        db = await idb.openDB(STORE_NAME_1,2,{
-            upgrade: function (upgradeDb,oldVersion, newVersion) {
-                if (upgradeDb.objectStoreNames.contains(STORE_NAME_1) == null){
-                    let userDataDB = upgradeDb.createObjectStore(STORE_NAME_1,{
-                        keyPath: 'roomID',
-                        autoIncrement: false
-                    });
-                    userDataDB.createIndex('roomID','roomID',{unique:true,multiEntry:true})
+    if (db == null) {
+        db = await idb.openDB(STORE_NAME_1, 2, {
+            upgrade: function (upgradeDb, oldVersion, newVersion) {
+                if (upgradeDb.objectStoreNames.contains(STORE_NAME) == null) {
+                    let userDataDB = upgradeDb.createObjectStore(STORE_NAME,
+                        {keyPath: "id", autoIncrement: true});
+                    userDataDB.createIndex('imageUrl', 'imageUrl', {unique: false});
+                    userDataDB.createIndex('roomID', 'roomID', {unique: false, autoIncrement: false});
+                    userDataDB.createIndex('annotations', 'annotations', {unique: false});
+                    userDataDB.createIndex('chat', 'chat', {unique: false});
                 }
-                if (upgradeDb.objectStoreNames.contains(STORE_NAME_2) == null){
-                    let userDataDB = upgradeDb.createObjectStore(STORE_NAME_2,{
-                        keyPath: 'ID',
-                        autoIncrement: true
-                        });
-                    userDataDB.createIndex('roomID','roomID',{unique:false,multiEntry:true})
-                }
-
             }
         });
         console.log('db is created');
     }
-
 }
 window.initDatabase = initDatabase;
