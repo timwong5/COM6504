53f1989f036cb0442011c909fff57f8f2f8bcc64
Yining Wang <wangyining203@gmail.com>
2021-09-28 23:38:30 +0800

    add the socket-io.js

--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -1,8 +1,29 @@
 
 exports.init = function(io) {
-  io.sockets.on('connection', function (socket) {
+  const chat = io
+      .of('/chat')
+      .on('connection', function (socket) {
     try {
      // insert here your event
+      /**
+       * it creates or joins a room
+       */
+      socket.on('create or join', function (room, userId) {
+        socket.join(room);
+        chat.to(room).emit('joined', room, userId);
+      });
+
+      socket.on('chat', function (room, userId, chatText) {
+        chat.to(room).emit('chat', room, userId, chatText);
+      });
+
+      socket.on('disconnect', function(){
+        console.log('someone disconnected');
+      });
+      socket.on('draw', function(roomID, userId, canvasWidth, canvasHeight, prevX, prevY, currX, currY, color, thickness){
+        console.log('X:' + currX + '  Y:' + currY);
+        socket.broadcast.emit('drawing', roomID, userId, canvasWidth, canvasHeight, prevX, prevY, currX, currY, color, thickness);
+      });
     } catch (e) {
     }
   });
