d797fe46bb26a3abf87e4ce7c8e3ddf8ad5426a0
Yining Wang <wangyining203@gmail.com>
2021-10-02 21:35:52 +0800

    abstract functions in database.js

--- public/javascripts/database.js
+++ public/javascripts/database.js
@@ -34,87 +34,47 @@ async function initDatabase() {
 window.initDatabase = initDatabase;
 
 
-//@todo try to abstract these functions
-
-
-
-/**
- * store the chatData
- * @param chatData
- * @returns {Promise<void>}
- */
-async function storeChatData(chatData) {
-    console.log('inserting: '+JSON.stringify(chatData));
+//abstract these functions
+async function storeData(data, storeName){
+    console.log('inserting: '+JSON.stringify(data));
     if (db == null){
         await initDatabase();
         console.log('db is created');
     }
     else if (db){
         try {
-            let tx = await db.transaction(CHAT_STORE_NAME, 'readwrite');
-            let store = await tx.objectStore(CHAT_STORE_NAME);
+            let tx = await db.transaction(storeName, 'readwrite');
+            let store = await tx.objectStore(storeName);
             let index = await store.index('roomID');
 
             //if the request roomID is exist, store the chatData to the IndexedDB into the right room
-            let request = await index.getAll(IDBKeyRange.only(chatData.roomID));
+            let request = await index.getAll(IDBKeyRange.only(data.roomID));
             if (request.length > 0){
-                store.put(chatData, request.roomID);
+                store.put(data, request.roomID);
             }
             //else create a new record
             else{
-                store.put(chatData);
+                store.put(data);
             }
             await tx.done;
 
 
         }
         catch (error) {
-            console.log('error to store chat');
-        }
-    }
-
-}
-window.storeChatData = storeChatData;
-
-async function storeAnnotationData(annotationData) {
-    console.log('inserting: '+JSON.stringify(annotationData));
-    if (db == null){
-        await initDatabase();
-        console.log('db is created');
-    }
-    else if (db){
-        try {
-            let tx = await db.transaction(ANNOTATION_STORE_NAME, 'readwrite');
-            let store = await tx.objectStore(ANNOTATION_STORE_NAME);
-            let index = await store.index('roomID');
-
-            //if the request roomID is exist, store the chatData to the IndexedDB into the right room
-            let request = await index.getAll(IDBKeyRange.only(annotationData.roomID));
-            if (request.length > 0){
-                store.put(annotationData, request.roomID);
-            }
-            //else create a new record
-            else{
-                store.put(annotationData);
-            }
-            await tx.done;
-        }
-        catch (error) {
-            console.log('error to store annotation');
+            console.log('error to store');
         }
     }
 }
-window.storeAnnotationData = storeAnnotationData;
 
-async function getChatData(roomID) {
+async function getData(roomID, storeName){
     if (db == null){
         await initDatabase();
     }
     else if (db){
         try{
             console.log('get the chatHistory '+ roomID );
-            let tx = await db.transaction(CHAT_STORE_NAME, 'readonly');
-            let store = await tx.objectStore(CHAT_STORE_NAME);
+            let tx = await db.transaction(storeName, 'readonly');
+            let store = await tx.objectStore(storeName);
             let index = await store.index('roomID');
 
             let list = await index.getAll(IDBKeyRange.only(roomID));
@@ -133,37 +93,54 @@ async function getChatData(roomID) {
 
         }
     }
+}
+
+
+
+/**
+ * store the chatData
+ * @param chatData
+ * @returns {Promise<void>}
+ */
+async function storeChatData(chatData) {
+    await storeData(chatData, CHAT_STORE_NAME);
+
+}
+window.storeChatData = storeChatData;
+
+
+/**
+ * store the annotation data
+ * @param annotationData
+ * @returns {Promise<void>}
+ */
+async function storeAnnotationData(annotationData) {
+    await storeData(annotationData, ANNOTATION_STORE_NAME);
+}
+window.storeAnnotationData = storeAnnotationData;
+
+
+
+/**
+ * get the chatData
+ * @param roomID
+ * @returns {Promise<void>}
+ */
+async function getChatData(roomID) {
+    await getData(roomID, CHAT_STORE_NAME);
 
 }
 window.getChatData = getChatData;
 
+
+
+/**
+ * get the annotation data
+ * @param roomID
+ * @returns {Promise<void>}
+ */
 async function getAnnotationData(roomID) {
-    if (db == null){
-        await initDatabase();
-    }
-    else if (db){
-        try{
-            console.log('get the annotation '+ roomID );
-            let tx = await db.transaction(ANNOTATION_STORE_NAME, 'readonly');
-            let store = await tx.objectStore(ANNOTATION_STORE_NAME);
-            let index = await store.index('roomID');
-
-            let list = await index.getAll(IDBKeyRange.only(roomID));
-            await tx.done;
-            //set the array for chatHistory
-            if (list.length > 0){
-                return list;
-            }
-            else{
-                console.log('error to get annotation history');
-            }
-        }
-        catch (error) {
-            console.log('error to get annotation history');
-        }{
-
-        }
-    }
+    await getData(roomID, ANNOTATION_STORE_NAME);
 
 }
 window.getAnnotationData = getAnnotationData;
